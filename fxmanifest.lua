0a2dfa7a1240e7fdfe28ce1bb765984793af1819
ariuky5 <87450459+ariuky5@users.noreply.github.com>
2021-07-22 21:05:15 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,19 @@
 fx_version 'adamant'
 game 'gta5'
+
+author "Pa1nlessz#2021 and ariuky#3449"
+
 description "A simple starterpack for esx"
 
-client_scripts {"*.lua"}
+client_scripts {
+  "*.lua"
+}
 
-shared_scripts {"config.lua"}
+shared_scripts {
+  "config.lua"
+}
 
-server_script {"*.lua"}
+server_script {
+  "*.lua"
+}
+
