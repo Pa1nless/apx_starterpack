618fda6a27232c77cc4ba2bf749aefdf7c42329d
Pa1nless <72404226+Pa1nless@users.noreply.github.com>
2021-07-22 15:03:32 -0600

    Feat: optimized client

--- apx_starterpack/client/client.lua
+++ apx_starterpack/client/client.lua
@@ -1,4 +1,3 @@
-
 ESX = nil
 
 Citizen.CreateThread(function()
@@ -10,3 +9,15 @@ Citizen.CreateThread(function()
     SpawnNPC()
 end)
 
+Citizen.CreateThread(function()
+    while true do 
+        local _apx = 250
+        local _ped = PlayerPedId()
+        local _pedpos = GetEntityCoords(_ped)
+        if GetDistanceBetweenCoords(_pedpos, Config.Coords.x, Config.Coords.y, Config.Coords.z, true) < 3 then
+            _apx = 0
+            FloatTxT(Config.Text, vector3(Config.Coords.x, Config.Coords.y, Config.Coords.z + 1.8))
+        end
+        Citizen.Wait(_apx)
+    end
+end)
