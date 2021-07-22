2a159b815da4a18afb86e0b587343606b2dc2535
Pa1nless <72404226+Pa1nless@users.noreply.github.com>
2021-07-22 14:45:27 -0600

    Feat: Ped already Working, client almost finished

--- apx_starterpack/client/client.lua
+++ apx_starterpack/client/client.lua
@@ -1,9 +1,12 @@
+
 ESX = nil
 
 Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
-    end
+    end 
+    ESX.PlayerData = ESX.GetPlayerData()
+    SpawnNPC()
 end)
 
