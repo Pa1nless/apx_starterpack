bf5a089d58e6d6910cfffdb7b69a332ca80560c3
ariuky5 <87450459+ariuky5@users.noreply.github.com>
2021-07-22 21:27:04 +0100

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,20 @@
 fx_version 'adamant'
+
 game 'gta5'
 
 author "Pa1nlessz#2021 and ariuky#3449"
 
 description "A simple starterpack for esx"
 
+
 client_scripts {
-  "*.lua"
+  'client/*.lua'
+}
+
+server_scripts {
+  'server/*.lua'
 }
 
 shared_scripts {
-  "config.lua"
+  'config.lua'
 }
-
-server_script {
-  "*.lua"
-}
-
