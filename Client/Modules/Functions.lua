20d12dfd032b2d9db63d4a207326edf22bc4017a
Pa1nless <72404226+Pa1nless@users.noreply.github.com>
2021-07-24 14:20:16 -0600

    Update Functions.lua

--- Client/Modules/Functions.lua
+++ Client/Modules/Functions.lua
@@ -21,10 +21,10 @@ end
 
 FloatTxT = function(msg, coords)
     AddTextEntry('esxFloatingHelpNotification', msg)
-	SetFloatingHelpTextWorldPosition(1, coords)
-	SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
-	BeginTextCommandDisplayHelp('esxFloatingHelpNotification')
-	EndTextCommandDisplayHelp(2, false, false, -1)
+   SetFloatingHelpTextWorldPosition(1, coords)
+   SetFloatingHelpTextStyle(1, 1, 2, -1, 3, 0)
+   BeginTextCommandDisplayHelp('esxFloatingHelpNotification')
+   EndTextCommandDisplayHelp(2, false, false, -1)
 end
 
 Menu = function()
